e15f1c85f82e1a27486ba2610674b3be1a3a5233
Ursulina Kölbener <ursulina@koelbener.ch>
2023-04-26 21:10:38 +0200

    accessing data

--- scripts/01-access data.R
+++ scripts/01-access data.R
@@ -6,14 +6,31 @@ library(jsonlite)
 # access data from https://opendata.swiss/de/dataset/polizeilich-registrierte-straftaten-gemass-strafgesetzbuch-nach-kanton-ausfuhrungsgrad-und-aufk5
 url <- "https://www.pxweb.bfs.admin.ch/api/v1/de/px-x-1903020100_101/px-x-1903020100_101.px"
 
-# convert to json
+# convert from json
 straftaten <- fromJSON(url)
 
 # print names of variables
 names(straftaten$variables)
 
-df_straft <- list()
+# create a dataframe ----
 
-for(i in length(straftaten)){
-  df_straft[[i]] <- straftaten$variables$text
-}
+# extract the variables code from the straftaten list
+variables_code <- straftaten$variables$code
+
+# extract the variables 1 from the straftaten list
+variables_1 <- straftaten$variables$valueTexts[[1]]
+
+# extract the variables 2 from the straftaten list
+variables_2 <- straftaten$variables$valueTexts[[2]]
+
+# extract the variables 3 from the straftaten list
+variables_3 <- straftaten$variables$valueTexts[[3]]
+
+# extract the variables 4 from the straftaten list
+variables_4 <- straftaten$variables$valueTexts[[4]]
+
+# extract the variables 5 from the straftaten list
+variables_5 <- straftaten$variables$valueTexts[[5]]
+
+# the actual data is missing (Number of crimes)
+
