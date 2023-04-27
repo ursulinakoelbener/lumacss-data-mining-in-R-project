7d5d626bfe0b9a344db45fc9b48657bc741421df
Ursulina Kölbener <ursulina@koelbener.ch>
2023-04-27 11:26:57 +0200

    tidy data

--- scripts/02-access data_csv.R
+++ scripts/02-access data_csv.R
@@ -6,3 +6,15 @@ library(tidyverse)
 library(data.table)
 
 # load data
+df <- fread(here::here("data", "px-x-1903020100_101_20230427-102332.csv"), header = TRUE)
+
+# make data tidy
+df_strft <- df %>% 
+  mutate(`2021` = as.character(`2021`)) %>%
+  mutate(`2022` = as.character(`2022`)) %>%
+  pivot_longer(cols = c(`2009`, `2010`, `2011`, `2012`, `2013`, `2014`,`2015`, 
+                        `2016`,`2017`, `2018`, `2019`, `2020`, `2021`, `2022`),
+               names_to = "Jahr",
+               values_to = "Value") %>%
+  mutate(Value = as.numeric(Value)) %>% # change to numeric
+  mutate(Jahr = as.numeric(Jahr)) # change to numeric
